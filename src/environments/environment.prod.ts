fc0f9aefa9a34187e0b30b8ff881602deb60b7aa
AyaaMostafa <aya.mostafa@remain.com.sa>
2026-02-16 16:17:35 +0200

    synccc

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.codegenerator.com/api',
+    apiUrl: 'https://codertest.rsys.life:5555/api',
     appName: 'Code Generator',
     version: '1.0.0',
     defaultLanguage: 'en',
